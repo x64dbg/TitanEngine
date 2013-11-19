72b5f87d10fea3306e7d2f406d8f333b42756790
mr.exodia <mr.exodia.tpodt@gmail.com>
2013-11-19 22:10:42 +0100

    removed files from Release

--- Release/x32/lib2a.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set PATH=c:\MinGW64\bin
-gendef TitanEngine.dll
-dlltool --as-flags=--32 -m i386 -k --output-lib TitanEngine_x86.a --input-def TitanEngine.def
-del TitanEngine.def
--- Release/x64/lib2a.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set PATH=c:\MinGW64\bin
-gendef TitanEngine.dll
-dlltool --as-flags=--64 -m i386:x86-64 -k --output-lib TitanEngine_x64.a --input-def TitanEngine.def
-del TitanEngine.def
