ef9ed7ae1f8dff8c1b003f3a78b0c3b3f7a52919
NtQuery <NtQuery>
2014-03-04 20:32:26 +0100

    better hidedebugger code, started heap flag

--- TitanEngine/Global.Engine.Hider.cpp
+++ TitanEngine/Global.Engine.Hider.cpp
@@ -19,6 +19,24 @@ static bool isAtleastVista()
 	return isAtleastVista;
 }
 
+static bool isWindows64()
+{
+	SYSTEM_INFO si = {0};
+	typedef void (WINAPI *tGetNativeSystemInfo)(LPSYSTEM_INFO lpSystemInfo);
+	tGetNativeSystemInfo _GetNativeSystemInfo = (tGetNativeSystemInfo)GetProcAddress(GetModuleHandleA("kernel32.dll"), "GetNativeSystemInfo");
+
+	if (_GetNativeSystemInfo)
+	{
+		_GetNativeSystemInfo(&si);
+	}
+	else
+	{
+		GetSystemInfo(&si);
+	}
+
+	return (si.wProcessorArchitecture == PROCESSOR_ARCHITECTURE_AMD64);
+}
+
 void FixAntidebugApiInProcess32(HANDLE hProcess, bool Hide)
 {
 	const BYTE patchCheckRemoteDebuggerPresent[5] = {
@@ -29,37 +47,105 @@ void FixAntidebugApiInProcess32(HANDLE hProcess, bool Hide)
 		0x33, 0xC0, //XOR EAX,EAX
 		0xC3}; //RETN
 
-	ULONG_PTR APIPatchAddress = NULL;
-	DWORD OldProtect;
+	ULONG_PTR APIPatchAddress = 0;
+	DWORD OldProtect = 0;
 	SIZE_T ueNumberOfBytesRead = 0;
 
 	if(Hide)
 	{
 		APIPatchAddress = (ULONG_PTR)EngineGlobalAPIHandler(hProcess, NULL, (ULONG_PTR)GetProcAddress(GetModuleHandleA("kernel32.dll"),"CheckRemoteDebuggerPresent"), NULL, UE_OPTION_IMPORTER_REALIGN_APIADDRESS);
 
-		VirtualProtectEx(dbgProcessInformation.hProcess, (LPVOID)APIPatchAddress, sizeof(patchCheckRemoteDebuggerPresent), PAGE_EXECUTE_READWRITE, &OldProtect);
-		WriteProcessMemory(hProcess, (LPVOID)(APIPatchAddress), &patchCheckRemoteDebuggerPresent, sizeof(patchCheckRemoteDebuggerPresent), &ueNumberOfBytesRead);
-		VirtualProtectEx(dbgProcessInformation.hProcess, (LPVOID)APIPatchAddress, sizeof(patchCheckRemoteDebuggerPresent), OldProtect, &OldProtect);
+		if (VirtualProtectEx(dbgProcessInformation.hProcess, (LPVOID)APIPatchAddress, sizeof(patchCheckRemoteDebuggerPresent), PAGE_EXECUTE_READWRITE, &OldProtect))
+		{
+			WriteProcessMemory(hProcess, (LPVOID)(APIPatchAddress), &patchCheckRemoteDebuggerPresent, sizeof(patchCheckRemoteDebuggerPresent), &ueNumberOfBytesRead);
+			VirtualProtectEx(dbgProcessInformation.hProcess, (LPVOID)APIPatchAddress, sizeof(patchCheckRemoteDebuggerPresent), OldProtect, &OldProtect);
+		}
+
 
 		APIPatchAddress = (ULONG_PTR)EngineGlobalAPIHandler(hProcess, NULL, (ULONG_PTR)GetProcAddress(GetModuleHandleA("kernel32.dll"),"GetTickCount"), NULL, UE_OPTION_IMPORTER_REALIGN_APIADDRESS);
 
-		VirtualProtectEx(dbgProcessInformation.hProcess, (LPVOID)APIPatchAddress, sizeof(patchGetTickCount), PAGE_EXECUTE_READWRITE, &OldProtect);
-		WriteProcessMemory(hProcess, (LPVOID)(APIPatchAddress), &patchGetTickCount, sizeof(patchGetTickCount), &ueNumberOfBytesRead);
-		VirtualProtectEx(dbgProcessInformation.hProcess, (LPVOID)APIPatchAddress, sizeof(patchGetTickCount), OldProtect, &OldProtect);
+		if (VirtualProtectEx(dbgProcessInformation.hProcess, (LPVOID)APIPatchAddress, sizeof(patchGetTickCount), PAGE_EXECUTE_READWRITE, &OldProtect))
+		{
+			WriteProcessMemory(hProcess, (LPVOID)(APIPatchAddress), &patchGetTickCount, sizeof(patchGetTickCount), &ueNumberOfBytesRead);
+			VirtualProtectEx(dbgProcessInformation.hProcess, (LPVOID)APIPatchAddress, sizeof(patchGetTickCount), OldProtect, &OldProtect);
+		}
 	}
 	else
 	{
 		APIPatchAddress = (ULONG_PTR)EngineGlobalAPIHandler(hProcess, NULL, (ULONG_PTR)GetProcAddress(GetModuleHandleA("kernel32.dll"),"CheckRemoteDebuggerPresent"), NULL, UE_OPTION_IMPORTER_REALIGN_APIADDRESS);
 
-		VirtualProtectEx(dbgProcessInformation.hProcess, (LPVOID)APIPatchAddress, sizeof(patchCheckRemoteDebuggerPresent), PAGE_EXECUTE_READWRITE, &OldProtect);
-		WriteProcessMemory(hProcess, (LPVOID)(APIPatchAddress), (void*)GetProcAddress(GetModuleHandleA("kernel32.dll"),"CheckRemoteDebuggerPresent"), sizeof(patchCheckRemoteDebuggerPresent), &ueNumberOfBytesRead);
-		VirtualProtectEx(dbgProcessInformation.hProcess, (LPVOID)APIPatchAddress, sizeof(patchCheckRemoteDebuggerPresent), OldProtect, &OldProtect);
+		if (VirtualProtectEx(dbgProcessInformation.hProcess, (LPVOID)APIPatchAddress, sizeof(patchCheckRemoteDebuggerPresent), PAGE_EXECUTE_READWRITE, &OldProtect))
+		{
+			WriteProcessMemory(hProcess, (LPVOID)(APIPatchAddress), (void*)GetProcAddress(GetModuleHandleA("kernel32.dll"),"CheckRemoteDebuggerPresent"), sizeof(patchCheckRemoteDebuggerPresent), &ueNumberOfBytesRead);
+			VirtualProtectEx(dbgProcessInformation.hProcess, (LPVOID)APIPatchAddress, sizeof(patchCheckRemoteDebuggerPresent), OldProtect, &OldProtect);
+		}
 
 		APIPatchAddress = (ULONG_PTR)EngineGlobalAPIHandler(hProcess, NULL, (ULONG_PTR)GetProcAddress(GetModuleHandleA("kernel32.dll"),"GetTickCount"), NULL, UE_OPTION_IMPORTER_REALIGN_APIADDRESS);
 
-		VirtualProtectEx(dbgProcessInformation.hProcess, (LPVOID)APIPatchAddress, sizeof(patchGetTickCount), PAGE_EXECUTE_READWRITE, &OldProtect);
-		WriteProcessMemory(hProcess, (LPVOID)(APIPatchAddress), (void*)GetProcAddress(GetModuleHandleA("kernel32.dll"),"GetTickCount"), sizeof(patchGetTickCount), &ueNumberOfBytesRead);
-		VirtualProtectEx(dbgProcessInformation.hProcess, (LPVOID)APIPatchAddress, sizeof(patchGetTickCount), OldProtect, &OldProtect);
+		if (VirtualProtectEx(dbgProcessInformation.hProcess, (LPVOID)APIPatchAddress, sizeof(patchGetTickCount), PAGE_EXECUTE_READWRITE, &OldProtect))
+		{
+			WriteProcessMemory(hProcess, (LPVOID)(APIPatchAddress), (void*)GetProcAddress(GetModuleHandleA("kernel32.dll"),"GetTickCount"), sizeof(patchGetTickCount), &ueNumberOfBytesRead);
+			VirtualProtectEx(dbgProcessInformation.hProcess, (LPVOID)APIPatchAddress, sizeof(patchGetTickCount), OldProtect, &OldProtect);
+		}
+	}
+}
+
+
+//Quote from The Ultimate Anti-Debugging Reference by Peter Ferrie
+//Flags field exists at offset 0x0C in the heap on the 32-bit versions of Windows NT, Windows 2000, and Windows XP; and at offset 0x40 on the 32-bit versions of Windows Vista and later.
+//Flags field exists at offset 0x14 in the heap on the 64-bit versions of Windows XP, and at offset 0x70 in the heap on the 64-bit versions of Windows Vista and later.
+//ForceFlags field exists at offset 0x10 in the heap on the 32-bit versions of Windows NT, Windows 2000, and Windows XP; and at offset 0x44 on the 32-bit versions of Windows Vista and later.
+//ForceFlags field exists at offset 0x18 in the heap on the 64-bit versions of Windows XP, and at offset 0x74 in the heap on the 64-bit versions of Windows Vista and later.
+
+int getHeapFlagsOffset()
+{
+	if (isWindows64())
+	{
+		if (isAtleastVista())
+		{
+			return 0x70;
+		}
+		else
+		{
+			return 0x14;
+		}
+	}
+	else
+	{
+		if (isAtleastVista())
+		{
+			return 0x40;
+		}
+		else
+		{
+			return 0x0C;
+		}
+	}
+}
+
+int getHeapForceFlagsOffset()
+{
+	if (isWindows64())
+	{
+		if (isAtleastVista())
+		{
+			return 0x74;
+		}
+		else
+		{
+			return 0x18;
+		}
+	}
+	else
+	{
+		if (isAtleastVista())
+		{
+			return 0x44;
+		}
+		else
+		{
+			return 0x10;
+		}
 	}
 }
 
@@ -67,6 +153,10 @@ bool FixPebInProcess(HANDLE hProcess, bool Hide)
 {
 	PEB_CURRENT myPEB = {0};
 	SIZE_T ueNumberOfBytesRead = 0;
+	void * heapFlagsAddress = 0;
+	DWORD heapFlags = 0;
+	void * heapForceFlagsAddress = 0;
+	DWORD heapForceFlags = 0;
 
 #ifndef _WIN64
 	PEB64 myPEB64 = {0};
@@ -97,8 +187,9 @@ bool FixPebInProcess(HANDLE hProcess, bool Hide)
 			myPEB64.NtGlobalFlag &= ~0x70;
 #endif
 
-			//Fix heap flags: https://github.com/eschweiler/ProReversing
-			//BYTE* Heap = (BYTE*)myPEB.ProcessHeap;
+			heapFlagsAddress = (void *)((LONG_PTR)myPEB.ProcessHeap + getHeapFlagsOffset());
+			heapForceFlagsAddress = (void *)((LONG_PTR)myPEB.ProcessHeap + getHeapForceFlagsOffset());
+			//TODO finish Heap Flag Anti-Anti-Debug
 		}
 		else
 		{
